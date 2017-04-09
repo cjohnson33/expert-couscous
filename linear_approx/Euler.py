0a8932344a9294bb80ed7d2aac6e12032b5197e6
cjohnson33 <cjohnson4278@gmail.com>
2017-04-09 16:25:53 -0500

    Major rework of Euler.py
    Completely encapsulated version of Euler.py
    Added Print option
    Created Functions for:
    1. Euler's method (the actual computation)
    2. Print Menu
    3. Input form for base functions

--- linear_approx/Euler.py
+++ linear_approx/Euler.py
@@ -1,21 +1,42 @@
-from __future__ import division
-#obtain function, initial values, goal value, and iterations/steps
 def eulers():
-    f = raw_input("Enter equation to be approximated \n y'= ")
-    def funcF(a,b):
-        x = a
-        y = b
-        return eval(f)
-    x = float(eval(raw_input("Enter inital x value\n ")))
-    y = float(eval(raw_input("Enter inital y value\n ")))
-    g = float(eval(raw_input("Enter x value to approximate y at\n ")))
-    i = eval((raw_input("Enter a positive integer for the number of steps\n ")))
-    while type(i) != int or i <=0:
-        i = eval((raw_input("Enter a positive integer for the number of steps\n ")))
-    h = (g - x) / i
-    print '\n', "%-15s %-15s" % ('x=', 'y=')
-    for n in range(i):
-        print "%-15s %-15s" % (x, y)
-        y = y + h * funcF(x, y)
-        x = x + h
-    print "%-15s %-15s" % (x, y)
+    def userFunction(xValue,yValue, equation):
+        x = xValue
+        y = yValue
+        return eval(equation)
+
+    def eulersMethod(equation, x, y, xFinal, iterationCount, printOption):
+            stepSize = (xFinal - x) / float(iterationCount)
+            print '\n', "%-15s %-15s" % ('x=', 'y=')
+            for n in range(iterationCount):
+                if printOption == 1:
+                    print "%-15s %-15s" % (x, y)
+                y = y + stepSize * userFunction(x, y, equation)
+                x = x + stepSize
+            print "%-15s %-15s" % (x, y)
+
+    def printMenu():
+        done = False
+        while not done:
+            print "1. Print all values throughout computation"
+            print "2. Print only final approximation from method"
+            try:
+                selection = int(raw_input("Select an option: "))
+                if selection == 1:
+                    return 1
+                elif selection == 2:
+                    return 0
+                else:
+                    raise ValueError()
+            except ValueError:
+                print "\nInvalid Selection\n"
+
+    def BaseMethodInputs():
+        equation = raw_input("Enter equation to be approximated \n y'= ")
+        x = float(raw_input("Enter inital x value\n "))
+        y = float(raw_input("Enter inital y value\n "))
+        xFinal = float(raw_input("Enter x value to approximate y at\n "))
+        iterations = int(raw_input("Enter a positive integer for the number of steps\n "))
+        printSelection = printMenu()
+        return [equation, x, y, xFinal, iterations, printSelection]
+
+    eulersMethod(*BaseMethodInputs())
