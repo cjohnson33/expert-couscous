acf41f7b250506feffc5bbfa3b271285f65715e2
cjohnson33 <cjohnson4278@gmail.com>
2017-04-08 22:44:05 -0500

    Improved Mainmenu
    added exception handler and area to insert other programs

--- EulerProject.py
+++ EulerProject.py
@@ -1,7 +1,23 @@
 def mainmenu():
-    print "1. Euler's Method"
-    print "2. Improved Euler's Method"
-    print "3. Runge-Katta Method"
-    print "4. Quit Program"
+    done = False
+    while not done:
+        print "1. Euler's Method"
+        print "2. Improved Euler's Method"
+        print "3. Runge-Katta Method"
+        print "4. Quit Program"
+        try:
+            selection = int(raw_input("Select an option: "))
+            if selection == 1:
+                pass #insert euler's method function
+            elif selection == 2:
+                pass
+            elif selection == 3:
+                pass
+            elif selection == 4:
+                quit()
+            else:
+                raise ValueError()
+        except ValueError:
+            print "\nInvalid Selection \n"
 if __name__ == "__main__":
     mainmenu()
